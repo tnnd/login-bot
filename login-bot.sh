28a907bce2ef8441bd5f726dc77ffda944a41f26
cjli <lamchuanjiang@icloud.com>
2016-06-30 22:48:30 +0800

    添加定时任务和完善shell登录脚本

--- login-bot.sh
+++ login-bot.sh
@@ -1,1 +1,10 @@
-php -f 'login-bot.php'
+#! /bin/sh
+
+script_path='/root/login-bot/'
+log=${script_path}login-bot.log
+
+cd $script_path
+php -f ${script_path}login-bot.php
+
+echo "`date` => crond service started." >> $log
+
